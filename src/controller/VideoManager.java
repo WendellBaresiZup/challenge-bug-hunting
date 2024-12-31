f0efbd7b155ac936b7a0ea8bcae64a40be5739fb
Wendell Baresi <wendell.oliveira@zup.com.br>
2024-12-31 08:53:01 -0300

    Refatoração na classe Video Controller dentro do pacote controller,alterada para VideoManager

---
+++ src/controller/VideoManager.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.Video;
+import service.VideoService;
+
+public class VideoManager {
+
+    private final VideoService videoService;
+
+    public VideoManager(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    public void adicionarVideo(String titulo, String descricao, int duracao, String categoria, String dataPublicacao){
+        videoService.addVideo(new Video(titulo, descricao, duracao, categoria, dataPublicacao));
+    }
+
+
+}
